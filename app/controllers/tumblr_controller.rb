207287726ad7b2b61e7b9ae32f5ae07e54340186
Your Name <jonahparzen-johnson@Jonah-2.local>
2012-10-30 12:00:06 -0400

    tumblr markup

--- app/controllers/tumblr_controller.rb
+++ app/controllers/tumblr_controller.rb
@@ -7,12 +7,12 @@ require 'tumblr'
     Tumblr.blog = 'searchandrestore'
     @posts = Tumblr::Post.all
     
-     @tabs = About.all
+    @tabs = About.all
 
-     @press_clippings = PressClipping.
-        order('date DESC').
-        includes(:link).
-        limit(5)
+    @press_clippings = PressClipping.
+      order('date DESC').
+      includes(:link).
+      limit(5)
     
   end
 
